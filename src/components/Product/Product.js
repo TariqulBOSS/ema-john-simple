7152cd283eca46745a962e05eba785bad2f69f1e
Your Name <you@example.com>
2020-04-19 21:18:30 +0600

    shop page coding done

---
+++ src/components/Product/Product.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+
+const Product = (props) => {
+    //console.log(props)
+    const { img, name, seller, price, stock } = props.product;
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+
+            </div>
+
+            <div>
+
+
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>By:{seller}</small></p>
+                <p>${price}</p>
+
+                <p><small>Only {stock} left in stock - Order Soon</small></p>
+                  <button className="main-button"
+                  onClick={ () => props.handAddProduct(props.product)}
+                  > <FontAwesomeIcon icon={faShoppingCart} /> Add To Cart</button>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Product;
